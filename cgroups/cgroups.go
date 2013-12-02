bea6f572232ccb83c8e883ea98741e664ce45e29
Josh Poimboeuf <jpoimboe@redhat.com>
2013-12-02 16:21:41 -0600

    dockerinit: create a long-running dockerinit
    Create a long-running dockerinit which does some setup, starts the
    container app, waits for it to finish, and exits with the app's exit
    code.
    
    A long-running dockerinit is a prerequisite for using libvirt-lxc, since
    libvirt provides no way to return an exit code, and no way to split
    stdout and stderr into separate streams.
    
    It also will allow docker to get rid of the ugly Ghost state, and might
    even make it possible to do everything in the docker client and get rid
    of the docker daemon altogether.

--- cgroups/cgroups.go
+++ cgroups/cgroups.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/dotcloud/docker/mount"
+	"github.com/dotcloud/docker/utils"
 	"io"
 	"io/ioutil"
 	"os"
@@ -95,6 +96,20 @@ func GetPidsForContainer(id string) ([]int, error) {
 		if err != nil {
 			return pids, fmt.Errorf("Invalid pid '%s': %s", p, err)
 		}
+
+		// The .dockerinit process (pid 1) is an implementation detail,
+		// so don't add it to the pid list.
+		comm, err := ioutil.ReadFile(filepath.Join("/proc", strconv.Itoa(pid), "comm"))
+		if err != nil {
+			// Ignore any error, the process could have exited
+			// already.
+			utils.Debugf("can't read comm file for pid %d: %s", pid, err)
+			continue
+		}
+		if strings.TrimSpace(string(comm)) == ".dockerinit" {
+			continue
+		}
+
 		pids = append(pids, pid)
 	}
 	return pids, nil
